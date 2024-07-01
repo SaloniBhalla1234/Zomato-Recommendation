12b64e62919d13e301f6c80e5d2ba6c7553ad9b0
SaloniBhalla <salonibhalla07@gmail.com>
2024-07-01 15:45:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/recommendation")
+def recommendation():
+    return render_template('recommendation.html')
+
+
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0",port=5500)
